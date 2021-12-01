adbc7bd583cd3cf1ee33f14048b2589b38598684
Hengda Shi <>
2021-12-01 00:04:03 -0800

    add setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2021 Hengda Shi <hengda.shi@engineering.ucla.edu>
+#
+# Distributed under terms of the MIT license.
+
+"""
+setup
+"""
+
+from setuptools import setup, find_packages
+
+setup(name='lib', version='0.1', packages=find_packages())
